ba28d12b3c4da10f9cf53e3693f3ce0a2a9a6ba0
Станис <102852335+ImitoGSY@users.noreply.github.com>
2024-07-25 22:53:43 +1000

    DZ7\8

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Model;
 using Model.Runtime.Projectiles;
+using TMPro;
 using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEngine;
 using Utilities;
@@ -15,13 +16,24 @@ namespace UnitBrains.Player
         public override string TargetUnitName => "Cobra Commando";
         private const float OverheatTemperature = 3f;
         private const float OverheatCooldown = 2f;
+
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
+
         private bool _overheated;
+        private static int _idUnit = 0;
+        private const int _maxTargets = 3;
+
+        public int Id { get; private set; }
 
         public List<Vector2Int> OutOfRange = new();
 
-        
+        public SecondUnitBrain()
+        {
+            Id = ++_idUnit;
+        }
+
+
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
@@ -43,66 +55,43 @@ namespace UnitBrains.Player
 
         public override Vector2Int GetNextStep()
         {
-            Vector2Int target = OutOfRange[0];
-            Vector2Int nextPosition = Vector2Int.right;
-            if (OutOfRange.Count > 0 && !IsTargetInRange(target))
-            {
-                return unit.Pos.CalcNextStepTowards(target);
-            }
-
-            else
-            {
-                return unit.Pos;
-            }
+            Vector2Int target;
+            target = OutOfRange.Count > 0 ? OutOfRange[0] : unit.Pos;
+            return IsTargetInRange(target) ? unit.Pos : unit.Pos.CalcNextStepTowards(target);
         }
 
         protected override List<Vector2Int> SelectTargets()
         {
-            List<Vector2Int> result = GetReachableTargets();
-            Vector2Int targetEn = Vector2Int.zero;
-
+            List<Vector2Int> result = new List<Vector2Int>();
             OutOfRange.Clear();
 
-            float minD = float.MaxValue;
-
-
-            foreach (var target in GetAllTargets())
+            foreach (Vector2Int target in GetAllTargets())
             {
-                float dis = DistanceToOwnBase(target);
-                if (dis < minD)
-                {
-                    minD = dis;
-                    targetEn = target;
-                }
-            }
-
-
-            if (IsTargetInRange(targetEn))
-            {
-                result.Add(targetEn);
-            }
-            else
-            {
-                OutOfRange.Add(targetEn);
-            }
-
-
-            if (result.Count == 0 && OutOfRange.Count == 0)
-            {
-                var target = runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId];
-                if (IsTargetInRange(target))
-                {
-                    result.Add(target);
-                    return result;
-                }
-
                 OutOfRange.Add(target);
-                return result;
             }
-            else
+
+
+            if (OutOfRange.Count == 0)
             {
-                return result;
+                OutOfRange.Add(runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId]);
             }
+            SortByDistanceToOwnBase(OutOfRange);
+
+            int targetIndex = Id % _maxTargets;
+
+            if (targetIndex < OutOfRange.Count)
+                {
+                result.Add(OutOfRange[targetIndex]);
+                }
+            else
+                {
+                result.Add(OutOfRange[OutOfRange.Count - 1]);
+
+            }
+
+            return result;
+        
+
         }
 
 
