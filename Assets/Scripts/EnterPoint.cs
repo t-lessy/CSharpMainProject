fdb2c812cd27421a440a90d06c82f34171979321
Tamerlan <tamerlantoleukhanov@gmail.com>
2025-02-17 01:38:20 +0500

    1
    1

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,7 +11,7 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
-    //привет лол
+    
     void Start()
     {
         Time.timeScale = _timeScale;
