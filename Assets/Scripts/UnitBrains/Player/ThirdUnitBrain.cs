0671e29e3d43ff101b2f073a974af7ac420049f9
vadimlidzhiev <aulidziev@gmail.com>
2025-03-22 18:10:36 +0100

    added ThirdUnitBrain.cs
    Added "Ironclad Behemoth" unit logic (either drive or shoot, with 1 sec switch), changed Attack Delay from 0.75 to 0.15.

---
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -0,0 +1,78 @@
+﻿using Model.Runtime.Projectiles;
+using System.Collections.Generic;
+using UnitBrains.Player;
+using UnityEngine;
+
+namespace Assets.Scripts.UnitBrains.Player
+{
+    public class ThirdUnitBrain : DefaultPlayerUnitBrain
+    {
+        public override string TargetUnitName => "Ironclad Behemoth";
+        private enum UnitMode
+        {
+            Driving,
+            Shooting,
+            Switching
+        }
+        private UnitMode _currentMode = UnitMode.Driving;
+        private float _switchTimer = 0f;
+        private const float SWITCH_DURATION = 1f;
+        private UnitMode _nextMode;
+
+        protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
+        {
+            if (_currentMode == UnitMode.Shooting) base.GenerateProjectiles(forTarget, intoList);
+
+        }
+
+        public override Vector2Int GetNextStep()
+        {
+            if (_currentMode == UnitMode.Switching || _currentMode == UnitMode.Shooting)
+            {
+                return unit.Pos;
+            }
+            return base.GetNextStep();
+        }
+
+        protected override List<Vector2Int> SelectTargets()
+        {
+            if (_currentMode == UnitMode.Driving || _currentMode == UnitMode.Switching)
+            {
+                return new List<Vector2Int>();
+            }
+            return base.SelectTargets();
+        }
+
+        public override void Update(float deltaTime, float time)
+        {
+            base.Update(deltaTime, time);
+
+            switch (_currentMode)
+            {
+                case UnitMode.Switching:
+                    _switchTimer += deltaTime;
+                    if (_switchTimer >= SWITCH_DURATION)
+                        _currentMode = _nextMode;
+                    break;
+
+                case UnitMode.Driving:
+                    if (HasTargetsInRange())
+                        SwitchTo(UnitMode.Shooting);
+
+                    break;
+
+                case UnitMode.Shooting:
+                    if (!HasTargetsInRange())
+                        SwitchTo(UnitMode.Driving);
+                    break;
+            }
+        }
+
+        private void SwitchTo(UnitMode newMode)
+        {
+            _currentMode = UnitMode.Switching;
+            _nextMode = newMode;
+            _switchTimer = 0f;
+        }
+    }
+}
