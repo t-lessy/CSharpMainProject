109ba2d4cb2f9834b4af3ad7ab5b65bb36b876d5
Lankan <s.o.a.m.o.n.e.y.24.gmail.com>
2025-03-25 18:17:16 +0100

    ДОМАШНЕЕ ЗАДАНИЕ №4 | ХАСАНОВА АРИНА ( Исправление 2)

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -12,28 +12,28 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            
+
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
             ///////////////////////////////////////           
-           
-               //Если нынешняя температура больше или равна темпераратуры перегрева -> Прерываем метод
-               if(GetTemperature() >= overheatTemperature )
-               return;
 
-               for(int i = 0 ; i < GetTemperature() ; i++ ) 
-               {  
-                   // Создаем снаряд
-                   var projectile = CreateProjectile(forTarget);
-                   AddProjectileToList(projectile, intoList); 
-               } 
-               
-               IncreaseTemperature(); // повышаем температуру до перегрева
-               
+            //Если нынешняя температура больше или равна темпераратуры перегрева -> Прерываем метод
+            if (GetTemperature() >= overheatTemperature)
+                return;
+
+            for (int i = 0; i < GetTemperature(); i++)
+            {
+                // Создаем снаряд
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+
+            IncreaseTemperature(); // повышаем температуру до перегрева
+
             ///////////////////////////////////////
         }
 
@@ -48,24 +48,21 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            
-             List<Vector2Int> result = GetReachableTargets();
-
-             float minDistance = float.MaxValue;
-             Vector2Int nearTarget = Vector2Int.zero; // коардинаты для ближайшей цели изначально задаем как (0,0)
+            List<Vector2Int> result = GetReachableTargets();
+            float minDistance = float.MaxValue;
+            Vector2Int nearTarget = Vector2Int.zero; // коардинаты для ближайшей цели изначально задаем как (0,0)
 
             foreach (Vector2Int target in result) // Проходим по всем существующим целям и ищем ближайшую
-            {  
+            {
                 float distance = DistanceToOwnBase(target);
-                if( distance < minDistance) 
+                if (distance < minDistance)
                 {
                     minDistance = distance;
                     nearTarget = target; // Устанавливаем ближайшую цель
                 }
-            } 
+            }
             result.Clear(); // Очищаем список, чтобы вложить новую ближайшую  цель
-            
-            if (minDistance < float.MaxValue) 
+            if (minDistance < float.MaxValue)
             {
                 result.Add(nearTarget); //Добавлям в очищенный список новую цель
             }
@@ -76,9 +73,9 @@ namespace UnitBrains.Player
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -90,7 +87,7 @@ namespace UnitBrains.Player
 
         private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
 
