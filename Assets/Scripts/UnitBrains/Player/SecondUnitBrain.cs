2b23a7cc41f3d9a27beedbd4eaca67f4846ce963
Merlinooon <alexey.cheryatnickov@gmail.com>
2025-05-02 18:59:08 +0300

    ДЗ№4.Черятников А.В.(HW Most Dangerous Target)

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
+using Model;
+
 
 //using System.Diagnostics;
 using Model.Runtime.Projectiles;
 using UnityEditor.Graphs;
 using UnityEngine;
+using Utilities;
 using static UnityEditor.PlayerSettings;
+using static UnityEngine.GraphicsBuffer;
 
 namespace UnitBrains.Player
 {
@@ -18,6 +24,7 @@ namespace UnitBrains.Player
         private float _cooldownTime = 0f;
         private bool _overheated;
 
+        private List<Vector2Int> unreachebleTarget = new();
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
@@ -41,7 +48,39 @@ namespace UnitBrains.Player
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+
+            List<Vector2Int> unreachebleTarget = SelectTargets();
+            List<Vector2Int> allTargets = SelectTargets();
+            List<Vector2Int> result = SelectTargets();
+
+            Vector2Int position = unit.Pos;
+            Vector2Int Nextposition = Vector2Int.right;
+
+            //return base.GetNextStep();
+
+            // Если нет достижимых целей вообще, идем к недостижимой цели
+            if (result==null || result.Count==0)
+            {
+                Debug.Log(" Если нет достижимых целей вообще, идем к недостижимой цели");
+                foreach(var target in unreachebleTarget)
+                {
+                    
+                    return position.CalcNextStepTowards(target);
+
+                }
+               
+            }
+            // Если цели в рэнджэ атаки есть, остаемся на месте
+            else if (result != null && result.Count > 0)
+            {
+                Debug.Log(" Если цели в рэнджэ атаки есть, остаемся на месте");
+                return unit.Pos;
+            }
+         
+            return position.CalcNextStepTowards(runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId]);
+
+
+
         }
 
         protected override List<Vector2Int> SelectTargets()
@@ -49,28 +88,51 @@ namespace UnitBrains.Player
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-
+            List<Vector2Int> allTargets =  GetAllTargets().ToList();
+            
+            List<Vector2Int> result = new();
             
 
-            var min = new List<float> { };
+            unreachebleTarget.Clear();
+
+            int enemyID = IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId;
+            Vector2Int enemyBase = runtimeModel.RoMap.Bases[enemyID];
+
             var MinimumDistance = float.MaxValue;
 
-            while (result.Count > 1)
+            if ( allTargets.Count > 1)
             {
                 Vector2Int IntermediateVector = new Vector2Int { };
-                foreach (var mainTarget in result)
+                foreach (var mainTarget in allTargets)
                 {
-
                     if (MinimumDistance > DistanceToOwnBase(mainTarget))
                     {
                         MinimumDistance = DistanceToOwnBase(mainTarget);
+                        
                         IntermediateVector = mainTarget;
                     }
                 }
-            
-                result.Clear();
-                result.Add(IntermediateVector);
+                if (IsTargetInRange(IntermediateVector))
+                {
+                    result.Clear();
+                    result.Add(IntermediateVector);
+                }
+                else
+                {
+       
+                    unreachebleTarget.Add(IntermediateVector);
+                }   
+                
+            }
+            else if(IsTargetInRange(enemyBase))
+            {
+                
+                result.Add(enemyBase);
+               
+            }
+            else
+            {
+                unreachebleTarget.Add(enemyBase);
             }
             return result;
             ///////////////////////////////////////
