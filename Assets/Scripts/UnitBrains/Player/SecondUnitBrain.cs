86e0226f101ce7ce085c18d55b1e15173aca6b12
Varr5 <nigor95.1@gmail.com>
2025-05-28 03:06:21 +0300

    Most Dangerous Target

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using GluonGui.Dialog;
+using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
+using Utilities;
 
 namespace UnitBrains.Player
 {
@@ -13,60 +17,91 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
+        private List<Vector2Int> priorityEnemy = new();
+
         
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
             float temperature = GetTemperature();
-            if (temperature >= OverheatTemperature)
+            if (temperature >= overheatTemperature)
             {
                 return;
             }
             for (int i = 0; i <= temperature; i++)
             {
-            var projectile = CreateProjectile(forTarget);
-            
-            AddProjectileToList(projectile, intoList);
+                var projectile = CreateProjectile(forTarget);
+
+                AddProjectileToList(projectile, intoList);
             }
             IncreaseTemperature();
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
             ///////////////////////////////////////  
             ///////////////////////////////////////
+
         }
+        
+        
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
-        }
+
+
+            if (priorityEnemy.Count == 0)
+                return unit.Pos;
+            else if (GetReachableTargets().Contains(priorityEnemy[0]))
+                return unit.Pos;
+            else
+                return unit.Pos.CalcNextStepTowards(priorityEnemy[0]);
+        }   
+            
 
         protected override List<Vector2Int> SelectTargets()
         {
+            priorityEnemy.Clear();
+            List<Vector2Int> result = GetAllTargets().ToList();
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            float minDistance = float.MaxValue;
-            Vector2Int closestEnemy = Vector2Int.zero;
-            
-        if (result.Count == 0)
+
+
+            if (result.Count > 1)
             {
-                return result;
-            }
-            foreach (Vector2Int enemy in result)
-            {
-                float distance = DistanceToOwnBase(enemy);
-                if (distance < minDistance)
+                float minDistance = float.MaxValue;
+                Vector2Int closestEnemy = new Vector2Int();
+
+
+                foreach (var enemy in result)
                 {
-                    minDistance = distance;
-                    closestEnemy = enemy;
+                    float distance = DistanceToOwnBase(enemy);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestEnemy = enemy;
+                    }
                 }
+
+                priorityEnemy.Add(closestEnemy);
+
+
+                if (GetReachableTargets().Contains(closestEnemy))
+                    result.Add(closestEnemy);
             }
-            result.Clear();
-            result.Add(closestEnemy);
-            return result;
+            else
+            {
+                result.Clear();
+                Vector2Int baseCoordinates = runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerId];
+                priorityEnemy.Add(baseCoordinates);
+                if (IsTargetInRange(baseCoordinates))
+                    result.Add(baseCoordinates);
+            }
+                return result;
             
+
         }
+
+        
 
         public override void Update(float deltaTime, float time)
         {
@@ -94,5 +129,6 @@ namespace UnitBrains.Player
             _temperature += 1f;
             if (_temperature >= OverheatTemperature) _overheated = true;
         }
+        
     }
 }
