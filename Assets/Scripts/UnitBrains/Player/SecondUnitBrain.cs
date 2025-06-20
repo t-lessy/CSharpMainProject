04b37974f3fda4d7cd5e940ecefa4305a577e354
mtasjov <matsjov@bk.ru>
2025-06-20 18:48:47 +0300

    HW Unit Shooting Mechanics
    HW 1.3

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -16,11 +16,18 @@ namespace UnitBrains.Player
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+            if (GetTemperature() >= overheatTemperature)
+            {
+                return;
+            }
+            IncreaseTemperature();
+            int CurrentTemp=GetTemperature();
+            for (int i = 0; i < CurrentTemp; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+            
             ///////////////////////////////////////
         }
 
