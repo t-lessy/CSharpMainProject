ec0d32b181226a7e930c38e5b242e16a2a68a412
Elena Kostyukhina <vilya.kizikay@gmail.com>
2025-07-14 23:19:59 +0300

    ДЗ №3 / Костюхина Елена (kizikay)

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -18,9 +18,22 @@ namespace UnitBrains.Player
             float overheatTemperature = OverheatTemperature;
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+            ///////////////////////////////////////
+            ///
+
+            float currentTemperature = GetTemperature();
+
+            if (currentTemperature >= overheatTemperature) {
+                return;
+            }
+            
+            for (int i=0; i <= currentTemperature; i++ ) {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+            
+
+            IncreaseTemperature();
             ///////////////////////////////////////
         }
 
