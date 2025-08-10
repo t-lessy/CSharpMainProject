ca97b9803c602a6bc13cfef86e9bb8e0fe535798
Shumskiy Ilya <ilya.shumskiy@simbirsoft.com>
2025-08-10 14:38:37 +0400

    homework 7

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,17 +1,29 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Model;
 using Model.Runtime.Projectiles;
 using UnityEngine;
+using Utilities;
 
 namespace UnitBrains.Player
 {
     public class SecondUnitBrain : DefaultPlayerUnitBrain
     {
+        public static int UnitCounter = 0;
         public override string TargetUnitName => "Cobra Commando";
         private const float OverheatTemperature = 3f;
         private const float OverheatCooldown = 2f;
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
+        private int _unitNumber;
+        private int _maxTargetCounter = 3;
+        private List<Vector2Int> _targets = new();
+        
+        public SecondUnitBrain()
+        {
+            _unitNumber = ++UnitCounter;
+        }
         
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
@@ -26,21 +38,49 @@ namespace UnitBrains.Player
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            if (_targets.Count > 0)
+            {
+                return unit.Pos.CalcNextStepTowards(_targets[_unitNumber % _targets.Count]);
+            }
+
+            return unit.Pos;
         }
 
         protected override List<Vector2Int> SelectTargets()
         {
-            ///////////////////////////////////////
-            // Homework 1.4 (1st block, 4rd module)
-            ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+            var result = new List<Vector2Int>();
+            var allTargets = GetAllTargets().ToList();
+
+            _targets.Clear();
+
+            if (allTargets.Count == 0)
+            {
+                var enemyBase = runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.PlayerId : RuntimeModel.BotPlayerId];
+                allTargets.Add(enemyBase);
+                return _targets;
+            }
+
+            SortByDistanceToOwnBase(allTargets);
+
+            for (var i = 0; i < Mathf.Min(_maxTargetCounter, allTargets.Count); i++)
+            {
+                if (IsTargetInRange(allTargets[i]))
+                {
+                    result.Add(allTargets[i]);
+                }
+                else
+                {
+                    _targets.Add(allTargets[i]);
+                }
+            }
+
+            if (result.Count > 0)
             {
                 result.RemoveAt(result.Count - 1);
+                _targets.AddRange(result);
             }
+
             return result;
-            ///////////////////////////////////////
         }
 
         public override void Update(float deltaTime, float time)
