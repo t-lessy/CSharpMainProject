a97521b4f50a1b2915175530835689cf5a4c9e97
Sergey-81 <s_nizhegorodov@mail.ru>
2025-08-12 06:23:58 +0300

    HW New Unit/ Нижегородов Сергей

--- Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,62 +8,46 @@ namespace UnitBrains.Player
     {
         public override string TargetUnitName => "Ironclad Behemoth";
 
-        private enum UnitState
-        {
-            Moving,
-            Attacking,
-            Switching
-        }
+        private static int _reloadingTimeSeconds = 1;
+        private static DateTime _reloadingStartDateTime;
 
-        private const float SwitchStateDurationSec = 1f;
-        private UnitState _currentState = UnitState.Moving;
-        private UnitState _nextState = UnitState.Moving;
-        private float _switchStartTime = 0f;
-
-        public override void Update(float deltaTime, float time)
-        {
-            base.Update(deltaTime, time);
-
-            if (_currentState == UnitState.Switching)
-            {
-                if (time - _switchStartTime >= SwitchStateDurationSec)
-                {
-                    _currentState = _nextState;
-                }
-                return;
-            }
-
-            bool hasTargets = HasTargetsInRange();
-
-            if (_currentState == UnitState.Moving && hasTargets)
-            {
-                StartSwitching(UnitState.Attacking, time);
-            }
-            else if (_currentState == UnitState.Attacking && !hasTargets)
-            {
-                StartSwitching(UnitState.Moving, time);
-            }
-        }
+        private bool _isReloading;
+        private bool _isBurstMode;
 
         public override Vector2Int GetNextStep()
         {
-            return _currentState != UnitState.Moving
-                ? unit.Pos
-                : base.GetNextStep();
+            if (NeedToSwitchMode()) StartModeSwitching();
+            return _isReloading || _isBurstMode ? unit.Pos : base.GetNextStep();
         }
 
         protected override List<Vector2Int> SelectTargets()
         {
-            return _currentState != UnitState.Attacking
-                ? new List<Vector2Int>()
-                : base.SelectTargets();
+            if (NeedToSwitchMode()) StartModeSwitching();
+            return _isReloading || !_isBurstMode ? new List<Vector2Int>() : base.SelectTargets();
         }
 
-        private void StartSwitching(UnitState newState, float currentTime)
+        public override void Update(float deltaTime, float time)
         {
-            _currentState = UnitState.Switching;
-            _nextState = newState;
-            _switchStartTime = currentTime;
+            if (_isReloading)
+            {
+                int totalSwitchingTime = (DateTime.Now - _reloadingStartDateTime).Seconds;
+                if (totalSwitchingTime >= _reloadingTimeSeconds) CompleteModeSwitching();
+            }
         }
+
+        private bool NeedToSwitchMode() => !_isReloading && (HasTargetsInRange() != _isBurstMode);
+
+        private void StartModeSwitching()
+        {
+            _reloadingStartDateTime = DateTime.Now;
+            _isReloading = true;
+        }
+
+        private void CompleteModeSwitching()
+        {
+            _isBurstMode = !_isBurstMode;
+            _isReloading = false;
+        }
+
     }
 }
