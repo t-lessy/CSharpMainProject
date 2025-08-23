bc506181b15e49e3b229e669231e6534c6cf9ad6
KashenkoKsu <Sakura.lur@mail.ru>
2025-08-23 13:56:51 +0300

    commit01

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -12,6 +12,14 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
     
+    //женялох
+    
+    /*/
+     * нет  он так не думает
+     * да он лох
+     * та за шо
+     * нюхай бебру
+    /*/
     void Start()
     {
         Time.timeScale = _timeScale;
