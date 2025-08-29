a5eba9cf3b9f609de0c7cd5ad555f850bfae2f52
KashenkoKsu <Sakura.lur@mail.ru>
2025-08-29 20:38:25 +0300

    commit13
    чото с дз

--- Assets/scrips/myfirstscripts.cs
+++ Assets/scrips/myfirstscripts.cs
@@ -9,20 +9,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int i = 7;
-        int f = 3;
-
-        bool isEqual = i == f;
-        bool isNotEqual = i != f;
-        bool isLessThan = i < f;
-        bool isGreaterThan = i > f;
-        bool isLessThanOrEqual = i <= f;
-        bool isGreaterThanOrEqual = i >= f;
-
-        Debug.Log(isEqual);
-        Debug.Log(isNotEqual);
-        Debug.Log(isLessThan);
-        Debug.Log(isGreaterThan);
+        bool isTrue = true;
+        bool isFalse = false;
+        bool and = isTrue && isFalse;
 
     }
 
