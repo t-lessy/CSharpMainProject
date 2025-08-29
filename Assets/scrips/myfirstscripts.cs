814e89eb5c578154c2480d7d4ce5532a40e103e0
KashenkoKsu <Sakura.lur@mail.ru>
2025-08-29 13:39:32 +0300

    commit11

--- Assets/scrips/myfirstscripts.cs
+++ Assets/scrips/myfirstscripts.cs
@@ -7,11 +7,13 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int i = 12;
-        float f = 3.2f;
-        string mommy = "IloveYouMommy@@@@@@@";
+        int i = 3;
+        int j = 2;
+        int k = i - j;
+        
+        Debug.Log(10 / k );
 
-        Debug.Log(mommy);  
+        Debug.Log("hi!pidori");
     }
 
 }
