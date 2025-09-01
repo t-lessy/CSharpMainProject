00a4b65114893e47caf9398c0cc1a890bfea749c
KashenkoKsu <Sakura.lur@mail.ru>
2025-09-01 12:40:28 +0300

    commit16
    dz#3

--- Assets/scrips/myfirstscripts.cs
+++ Assets/scrips/myfirstscripts.cs
@@ -9,9 +9,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bool isTrue = true;
-        bool isFalse = false;
-        bool and = isTrue && isFalse;
+   
 
     }
 
