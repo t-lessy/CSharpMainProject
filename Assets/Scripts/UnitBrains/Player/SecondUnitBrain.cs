f80356df58e3ae7c26c1d1712ae1c28614a5b97f
Admin <Admin@DESKTOP-589NA1G>
2025-09-06 22:49:16 +0300

    HM4

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using Model;
 using Model.Runtime.Projectiles;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Utilities;
 
 namespace UnitBrains.Player
 {
@@ -13,12 +15,13 @@ namespace UnitBrains.Player
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        
+        private List<Vector2Int> getAll = new List<Vector2Int>();
+
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
-           
 
-            if (GetTemperature () >= OverheatTemperature)
+
+            if (GetTemperature() >= OverheatTemperature)
             {
                 return;
             }
@@ -27,58 +30,127 @@ namespace UnitBrains.Player
 
             int countBullet = GetTemperature();
 
-            for (int i = 0; i < countBullet; i++) 
+            for (int i = 0; i < countBullet; i++)
             {
                 BaseProjectile projectile = CreateProjectile(forTarget);
                 AddProjectileToList(projectile, intoList);
             }
 
-           
-            
+
+
         }
 
+        //getAll.Last();
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+            if(getAll.Count > 0)
+            {
+                Vector2Int position = unit.Pos;
+                Vector2Int OneTarget = getAll.Last();
+                position = position.CalcNextStepTowards(OneTarget);
+                return position;
+            }
+
+            else {
+                return unit.Pos;
+            }
+                
         }
 
         protected override List<Vector2Int> SelectTargets()
         {
 
-            List<Vector2Int> result = GetReachableTargets();
-
+            List<Vector2Int> result = GetAllTargets().ToList();
+            getAll.Clear();
             if (result.Count == 0) 
             {
+                if (IsPlayerUnitBrain == true)
+                {
+                    int id = RuntimeModel.BotPlayerId;
+                    Vector2Int baseV = runtimeModel.RoMap.Bases[id];
+                    result.Add(baseV);
+                }
+                else
+                {
+                    int id = RuntimeModel.PlayerId;
+                    Vector2Int baseV = runtimeModel.RoMap.Bases[id];
+                    result.Add(baseV);
+                }
+
                 return result;
             }
+
+            
 
             float min = DistanceToOwnBase(result[0]);
             Vector2Int vector = result[0];
 
+            if (IsTargetInRange(result[0]) == false)
+            {
+                getAll.Add(result[0]);
+            }
+
             for (int i = 1; i < result.Count; i++)
             {
-                if (min < DistanceToOwnBase(result[i]))
+                if (IsTargetInRange(result[i]) == false)
+                {
+                    getAll.Add(result[i]);
+                }
+
+                if (min > DistanceToOwnBase(result[i]))
                 {
                     vector = result[i];
                     min = DistanceToOwnBase(result[i]);
                 }
+
+
+
             }
+
+            getAll.Add(vector);
 
             result.Clear();
 
-            result.Add(vector);
-            return result;
+            if (IsTargetInRange(vector) == true)
+            {
+                result.Add(vector);
+            }
+
+            else
+            {
+                if (IsPlayerUnitBrain == true)
+                {
+                    int id = RuntimeModel.BotPlayerId;
+                    Vector2Int baseV = runtimeModel.RoMap.Bases[id];
+                    result.Add(baseV);
+                }
+                else
+                {
+                    int id = RuntimeModel.PlayerId;
+                    Vector2Int baseV = runtimeModel.RoMap.Bases[id];
+                    result.Add(baseV);
+                }
+            }
 
 
-            ///////////////////////////////////////
+
+
+                return result;
+
+
+
+
         }
+
+
+
 
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -89,10 +161,10 @@ namespace UnitBrains.Player
         }
 
         private int GetTemperature()
-        { 
-            if(_overheated == true)
-                return (int)  OverheatTemperature;
-            else 
+        {
+            if (_overheated == true)
+                return (int)OverheatTemperature;
+            else
                 return (int)_temperature;
         }
 
