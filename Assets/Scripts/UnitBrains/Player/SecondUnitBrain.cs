14a854fbeb1e4dee27168ce26d1128390ae807ad
unknown <fos872@yandex.ru>
2025-10-12 04:16:52 +0700

    HW 4

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using Model;
 using Model.Runtime.Projectiles;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Utilities;
+using static UnityEngine.GraphicsBuffer;
 
 namespace UnitBrains.Player
 {
     public class SecondUnitBrain : DefaultPlayerUnitBrain
     {
+        Vector2Int one = new Vector2Int(1, 1);
         public override string TargetUnitName => "Cobra Commando";
+        private Vector2Int TargetsToAttack = new Vector2Int();
         private const float OverheatTemperature = 3f;
         private const float OverheatCooldown = 2f;
         private float _temperature = 0f;
@@ -26,20 +32,58 @@ namespace UnitBrains.Player
 
         public override Vector2Int GetNextStep()
         {
-            return base.GetNextStep();
+           return IsTargetInRange(TargetsToAttack) ? unit.Pos : unit.Pos.CalcNextStepTowards(TargetsToAttack);
         }
 
+        private Vector2Int GetCloserTarget(IEnumerable<Vector2Int> targets)
+        {
+            Vector2Int min_result = targets.First();
+
+            foreach (Vector2Int target in targets)
+            {
+                if (DistanceToOwnBase(min_result) > DistanceToOwnBase(target))
+                {
+                    min_result = target;
+                }
+            }
+            return min_result;
+        }
         protected override List<Vector2Int> SelectTargets()
         {
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            List<Vector2Int> result = GetReachableTargets();
-            while (result.Count > 1)
+            var allTargets = GetAllTargets();
+
+            List<Vector2Int> result = new List<Vector2Int>();
+
+            Vector2Int min_result = GetCloserTarget(allTargets);
+
+            if (IsTargetInRange(min_result))
             {
-                result.RemoveAt(result.Count - 1);
+                TargetsToAttack = min_result;
+                result.Add(min_result);
+            }
+            else
+            {
+                TargetsToAttack = min_result;
+            }
+
+            if (result.Count() == 0)
+            {
+                var targetBase = runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId];
+                if (!IsTargetInRange(targetBase))
+                {
+                    TargetsToAttack = targetBase;
+                }
+                else
+                { 
+                    TargetsToAttack = targetBase;
+                    result.Add(targetBase);
+                }
             }
             return result;
+
             ///////////////////////////////////////
         }
 
