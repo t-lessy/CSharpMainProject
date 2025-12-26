558007d32bfbfe5dda31b1d9305c9c5f369c8b0a
mtasjov <matsjov@bk.ru>
2025-12-26 17:28:22 +0300

    PW1_fix / @Abctrakt Даниил Чинаров

--- Assets/Scripts/UnitBrains/Pathfinding/AStarUnitPath.cs
+++ Assets/Scripts/UnitBrains/Pathfinding/AStarUnitPath.cs
@@ -1,89 +1,143 @@
 using Model;
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Net;
 using UnitBrains.Pathfinding;
 using UnityEngine;
 using Utilities;
 using System.Linq;
-//
+
 public class AStarUnitPath : BaseUnitPath
 {
-    private Vector2Int[] dxy = { new Vector2Int(-1, 0), new Vector2Int(0, 1), new Vector2Int(1, 0), new Vector2Int(0, -1) };
+    // Направления движения: влево, вверх, вправо, вниз
+    private Vector2Int[] dxy = {
+        new Vector2Int(-1, 0),
+        new Vector2Int(0, 1),
+        new Vector2Int(1, 0),
+        new Vector2Int(0, -1)
+    };
 
-    public AStarUnitPath(IReadOnlyRuntimeModel runtimeModel, Vector2Int startPoint, Vector2Int endPoint) : base(runtimeModel, startPoint, endPoint)
+    public AStarUnitPath(IReadOnlyRuntimeModel runtimeModel, Vector2Int startPoint, Vector2Int endPoint)
+        : base(runtimeModel, startPoint, endPoint)
     {
     }
 
     protected override void Calculate()
     {
         List<Vector2Int> result = FindPath();
-
         path = result.ToArray();
     }
 
     private List<Vector2Int> FindPath()
     {
-        PathNode startPathNode = new PathNode(startPoint);
-        PathNode targetPathNode = new PathNode(endPoint);
+        // Нода старта и цели
+        PathNode startNode = new PathNode(startPoint);
+        PathNode targetNode = new PathNode(endPoint);
 
-        List<PathNode> openList = new List<PathNode> { startPathNode };
-        List<PathNode> closedList = new List<PathNode>();
+        // Инициализируем стартовую ноду
+        startNode.GCost = 0; // g(n) = 0 для старта
+        startNode.CalculateHCost(endPoint); // Вычисляем h(n), для конечной
+        startNode.CalculateFCost(); // f(n) = g(n) + h(n)
+
+        // OpenList содержит ноды которе потом будут обобатываеться
+        List<PathNode> openList = new List<PathNode> { startNode };
+
+        // ClosedSet содержит уже обработанные 
+        HashSet<Vector2Int> closedSet = new HashSet<Vector2Int>();
 
         while (openList.Count > 0)
         {
-            PathNode currentPathNode = openList[0];
-
-            foreach (var PathNode in openList)
+            // Находим ноду с наименьшим f(n)
+            PathNode currentNode = openList[0];
+            for (int i = 1; i < openList.Count; i++)
             {
-                if (PathNode.Value < currentPathNode.Value)
-                    currentPathNode = PathNode;
-            }
-
-            openList.Remove(currentPathNode);
-            closedList.Add(currentPathNode);
-
-            if (currentPathNode.Position == targetPathNode.Position)
-            {
-                List<Vector2Int> path = new List<Vector2Int>();
-
-                while (currentPathNode != null)
+                // Выбор оптимальной ноды
+                if (openList[i].FCost < currentNode.FCost ||
+                    (openList[i].FCost == currentNode.FCost && openList[i].HCost < currentNode.HCost))
                 {
-                    path.Add(currentPathNode.Position);
-                    currentPathNode = currentPathNode.Parent;
+                    currentNode = openList[i];
                 }
-
-                path.Reverse();
-                return path;
             }
 
-            for (int i = 0; i < dxy.Length; i++)
+            // Удаляем из openList и добавляем в closedSet
+            openList.Remove(currentNode);
+            closedSet.Add(currentNode.Position);
+
+            // Если достигли цели - восстанавливаем путь
+            if (currentNode.Position == endPoint)
             {
-                Vector2Int newPos = currentPathNode.Position + dxy[i];
+                return ReconstructPath(currentNode);
+            }
 
-                if (runtimeModel.IsTileWalkable(newPos) || newPos == targetPathNode.Position || IsUnitAtPos(newPos))
+            // Проверяем всех соседей
+            foreach (var direction in dxy)
+            {
+                Vector2Int neighborPos = currentNode.Position + direction;
+
+                // Пропускаем уже обработанные позиции
+                if (closedSet.Contains(neighborPos))
+                    continue;
+
+                // Проверяем возможность пройти
+                bool isWalkable = runtimeModel.IsTileWalkable(neighborPos);
+                bool isTarget = neighborPos == endPoint;
+                bool hasUnit = IsUnitAtPos(neighborPos);
+
+                if (!isWalkable && !isTarget && !hasUnit)
+                    continue;
+
+                // Вычисляем стоимость движения на эту клетку
+                int moveCost = isWalkable ? 10 : 30;
+                // Новая g(n) = g(n) текущей ноды + стоимость движения
+                int newGCost = currentNode.GCost + moveCost;
+
+                // Ищем, есть ли эта позиция уже в openList
+                PathNode existingNode = openList.FirstOrDefault(n => n.Position == neighborPos);
+
+                if (existingNode == null)
                 {
-
-                    PathNode neighbor = new PathNode(newPos);
-                    if (closedList.Contains(neighbor) || openList.Contains(neighbor))
-                        continue;
-
-                    neighbor.Parent = currentPathNode;
-                    neighbor.CalculateEstimate(targetPathNode.Position);
-                    neighbor.CalculateValue();
-
-                    openList.Add(neighbor);
+                    // Создаем новую ноду
+                    PathNode newNode = new PathNode(neighborPos);
+                    newNode.GCost = newGCost; // Сумма стоимость от старта
+                    newNode.CalculateHCost(endPoint);
+                    newNode.CalculateFCost(); 
+                    newNode.Parent = currentNode; // Запоминаем откуда пришли
+                    openList.Add(newNode);
+                }
+                else if (newGCost < existingNode.GCost)
+                {
+                    // Если нашли более короткий путь - обновляем
+                    existingNode.GCost = newGCost;
+                    existingNode.CalculateFCost();
+                    existingNode.Parent = currentNode;
                 }
             }
         }
-        return new List<Vector2Int> { startPoint, startPoint };
+
+        // если не нашли путь, то возвращаем стартовую позицию
+        return new List<Vector2Int> { startPoint };
     }
 
+    // Восстанавливаем путь от цели к старту
+    private List<Vector2Int> ReconstructPath(PathNode endNode)
+    {
+        List<Vector2Int> path = new List<Vector2Int>();
+        PathNode currentNode = endNode;
+
+        // Идем от цели к старту по Parent
+        while (currentNode != null)
+        {
+            path.Add(currentNode.Position);
+            currentNode = currentNode.Parent;
+        }
+
+        // Реверс для того чтобы путь шел от старта к цели
+        path.Reverse();
+        return path;
+    }
+
+    // Проверяем, есть ли юнит на позиции
     private bool IsUnitAtPos(Vector2Int pos) =>
         runtimeModel.RoUnits.Any(u => u.Pos == pos);
-
 }
 
 public class PathNode
@@ -91,30 +145,41 @@ public class PathNode
     private IReadOnlyRuntimeModel runtimeModel => ServiceLocator.Get<IReadOnlyRuntimeModel>();
 
     public Vector2Int Position;
-    public int Cost;
-    public int Estimate;
-    public int Value;
+    public int GCost;
+    public int HCost;
+    public int FCost;
     public PathNode Parent;
 
     public PathNode(Vector2Int position)
     {
         Position = position;
-        Cost = runtimeModel.IsTileWalkable(Position) ? 10 : 30;
+        GCost = 0; // Будет установлено при добавлении в openList
     }
 
-    public void CalculateEstimate(Vector2Int target)
+    // Вычисляем h(n) - расстояние до цели
+    public void CalculateHCost(Vector2Int target)
     {
-        Estimate = Math.Abs(Position.x - target.x) + Math.Abs(Position.y - target.y);
+        HCost = Math.Abs(Position.x - target.x) + Math.Abs(Position.y - target.y);
     }
-    public void CalculateValue()
+
+    // Вычисляем f(n) = g(n) + h(n)
+    public void CalculateFCost()
     {
-        Value = Cost + Estimate;
+        FCost = GCost + HCost;
     }
-    public override bool Equals(object? obj)
+
+    // Переопределяем Equals для корректной работы с Contains и HashSet
+    public override bool Equals(object obj)
     {
-        if (obj is not PathNode PathNode)
+        if (obj is not PathNode other)
             return false;
 
-        return Position.x == PathNode.Position.x && Position.y == PathNode.Position.y;
+        return Position.x == other.Position.x && Position.y == other.Position.y;
+    }
+
+    // Переопределяем GetHashCode для работы с HashSet
+    public override int GetHashCode()
+    {
+        return Position.GetHashCode();
     }
 }
