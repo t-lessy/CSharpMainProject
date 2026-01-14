993cab7e343af61c482b398b38c6475d9916f300
Nekroubludok <drapezavova@gmail.com>
2026-01-14 17:13:46 +0200

    Final Changes

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -1,100 +1,95 @@
-﻿using System.Collections.Generic;
+﻿using Model;
 using Model.Runtime.Projectiles;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
+using Utilities;
 namespace UnitBrains.Player
 {
     public class SecondUnitBrain : DefaultPlayerUnitBrain
     {
+        Vector2Int one = new Vector2Int(1, 1);
         public override string TargetUnitName => "Cobra Commando";
+        private Vector2Int TargetsToAttack = new Vector2Int();
         private const float OverheatTemperature = 3f;
         private const float OverheatCooldown = 2f;
         private float _temperature = 0f;
         private float _cooldownTime = 0f;
         private bool _overheated;
-        public List<Vector2Int> result { get; set; }
-        public readonly List<Vector2Int> _currTarget = new();
 
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
             float overheatTemperature = OverheatTemperature;
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////
-            if (GetTemperature()<OverheatTemperature)
+            ///////////////////////////////////////           
+            
+            float cur_temp = GetTemperature();
+            if (cur_temp >= OverheatTemperature) return;
+            for (int i = 0; i <= cur_temp; i++)
             {
-                for (int i = 0; i <= GetTemperature(); i++)
-                {
-                    var projectile = CreateProjectile(forTarget);
-                    AddProjectileToList(projectile, intoList);
-                }
-                IncreaseTemperature(); 
-             
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
             }
+            IncreaseTemperature();
+            ///////////////////////////////////////
         }
-                ///////////////////////////////////////
 
         public override Vector2Int GetNextStep()
         {
-            if (_currTarget.Count > 0)
-            {
-                Vector2Int Target = _currTarget[0];
-            }
-            else
-            {
-                Target = unit.Pos;
-            }
-            return IsItPossibleToShootTarget(Target) ? unit.Pos : CalcNextStepTowards(Target);
+            return IsTargetInRange(TargetsToAttack) ? unit.Pos : unit.Pos.CalcNextStepTowards(TargetsToAttack);
         }
-        
+        private Vector2Int GetCloserTarget(IEnumerable<Vector2Int> targets)
+        {
+            Vector2Int min_result = targets.First();
+            foreach (Vector2Int target in targets)
+            {
+                if (DistanceToOwnBase(min_result) > DistanceToOwnBase(target))
+                {
+                    min_result = target;
+                }
+            }
+            return min_result;
+        }
         protected override List<Vector2Int> SelectTargets()
         {
             ///////////////////////////////////////
             // Homework 1.4 (1st block, 4rd module)
             ///////////////////////////////////////
-            var result = new List<Vector2Int>();
-            float lowestDistance = float.MaxValue;
-            Vector2Int nearestTarget = Vector2Int.zero;
-            foreach (var target in GetAllTargets())
+            var allTargets = GetAllTargets();
+            List<Vector2Int> result = new List<Vector2Int>();
+            Vector2Int min_result = GetCloserTarget(allTargets);
+            if (IsTargetInRange(min_result))
             {
-                if (DistanceToOwnBase(target) < lowestDistance)
-                {
-                    lowestDistance = DistanceToOwnBase(target);
-                    nearestTarget = target;
-                }
-            }
-            _currTarget.Clear();
-            if (lowestDistance < float.MaxValue)
-            {
-                _currTarget.Add(nearestTarget);
-                if (IsItPossibleToShootTarget(nearestTarget) == true)
-                {
-                    result.Add(nearestTarget);
-                }
-                
+                TargetsToAttack = min_result;
+                result.Add(min_result);
             }
             else
             {
-                result.Add(runtimeModel.RoMap.Bases[IsPlayerUnitBrain ? RuntimeModel.BotPlayerId : RuntimeModel.PlayerID]);
+                TargetsToAttack = min_result;
             }
-
+            if (result.Count() == 0)
+            {
+                var targetBase = runtimeModel.RoMap.Bases[RuntimeModel.BotPlayerId];
+                if (!IsTargetInRange(targetBase))
+                {
+                    TargetsToAttack = targetBase;
+                }
+                else
+                {
+                    TargetsToAttack = targetBase;
+                    result.Add(targetBase);
+                }
+            }
             return result;
-            
             ///////////////////////////////////////
         }
-
-        public bool IsItPossibleToShootTarget(Vector2Int mostDangerousEnemy)
-        {
-            List<Vector2Int> rectTargets = GetReachableTargets();
-            return rectTargets.Contains(mostDangerousEnemy);
-        }
-
         public override void Update(float deltaTime, float time)
         {
             if (_overheated)
-            {              
+            {
                 _cooldownTime += Time.deltaTime;
-                float t = _cooldownTime / (OverheatCooldown/10);
+                float t = _cooldownTime / (OverheatCooldown / 10);
                 _temperature = Mathf.Lerp(OverheatTemperature, 0, t);
                 if (t >= 1)
                 {
@@ -103,13 +98,11 @@ namespace UnitBrains.Player
                 }
             }
         }
-
         private int GetTemperature()
         {
-            if(_overheated) return (int) OverheatTemperature;
+            if (_overheated) return (int)OverheatTemperature;
             else return (int)_temperature;
         }
-
         private void IncreaseTemperature()
         {
             _temperature += 1f;
