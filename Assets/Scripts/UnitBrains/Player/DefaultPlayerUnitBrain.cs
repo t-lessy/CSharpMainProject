f00e73dbe8e7394d1deb4800489e820efac2e8b4
mtasjov <matsjov@bk.ru>
2026-01-18 16:18:15 +0300

    HW Singltones/ tg: @Abctrakt Даниил Чинаров

--- Assets/Scripts/UnitBrains/Player/DefaultPlayerUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/DefaultPlayerUnitBrain.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
-using Model;
+﻿using Model;
 using Model.Runtime.Projectiles;
+using System.Collections.Generic;
+using UnitBrains.Pathfinding;
 using UnityEngine;
 
 namespace UnitBrains.Player
 {
     public class DefaultPlayerUnitBrain : BaseUnitBrain
     {
+        public override BaseUnitPath ActivePath => _activePath;
+        private BaseUnitPath _activePath = null;
         protected float DistanceToOwnBase(Vector2Int fromPos) =>
             Vector2Int.Distance(fromPos, runtimeModel.RoMap.Bases[RuntimeModel.PlayerId]);
 
@@ -20,6 +23,37 @@ namespace UnitBrains.Player
             var distanceA = DistanceToOwnBase(a);
             var distanceB = DistanceToOwnBase(b);
             return distanceA.CompareTo(distanceB);
+        }
+
+        public override Vector2Int GetNextStep()
+        {
+
+            bool HasTargetsInDoubleRange()
+            {
+                var attackDoubleRangeSqr = (unit.Config.AttackRange * unit.Config.AttackRange) * 2;
+                foreach (var possibleTarget in GetAllTargets())
+                {
+                    var diff = possibleTarget - unit.Pos;
+                    if (diff.sqrMagnitude < attackDoubleRangeSqr)
+                        return true;
+                }
+
+                return false;
+            }
+
+
+            Vector2Int target = unit.Pos;
+
+            if (HasTargetsInRange())
+                return unit.Pos;
+
+            if (HasTargetsInDoubleRange())
+                target = UnitCoordinator.GetInstance().GetTarget();
+            else
+                target = UnitCoordinator.GetInstance().GetPoint();
+
+            _activePath = new AStarUnitPath(runtimeModel, unit.Pos, target);
+            return _activePath.GetNextStepFrom(unit.Pos);
         }
     }
 }
