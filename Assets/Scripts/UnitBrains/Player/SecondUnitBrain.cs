c110e727a5f5bbd574272a0fd21587050712a6a8
nkzhilin <nkzhilin@yandex.ru>
2026-02-02 23:16:50 +0400

    homework 1

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -18,9 +18,19 @@ namespace UnitBrains.Player
             float overheatTemperature = OverheatTemperature;
             ///////////////////////////////////////
             // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
+            ///////////////////////////////////////  
+            
+            if (GetTemperature() >= overheatTemperature)
+                    return;
+
+            IncreaseTemperature();
+            
+            for (int i = 0; i < _temperature; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+            
             ///////////////////////////////////////
         }
 
